1b1b6ebafe6735d8a194d155809083504bb2604a
Rogi-Vokamre <126895550+Rogi-Vokamre@users.noreply.github.com>
2025-09-10 21:31:10 +0300

    Create RingAnimation.js

---
+++ mini-app/src/components/RingAnimation/RingAnimation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./RingAnimation.css";
+
+function RingAnimation() {
+  return (
+    <div className="ring-container">
+      <div className="rotating-ring"></div>
+      <div className="rotating-ring outer-ring"></div>
+      <div className="rotating-ring outer-ring-2"></div>
+      <div className="rotating-ring outer-ring-3"></div>
+      <div className="rotating-ring outer-ring-4"></div>
+      <div className="rotating-ring outer-ring-5"></div>
+      <div className="rotating-ring outer-ring-6"></div>
+      <h1 className="center-text">SoMagic</h1>
+    </div>
+  );
+}
+
+export default RingAnimation;
