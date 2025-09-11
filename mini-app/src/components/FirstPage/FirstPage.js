6bb0d3da07feb34cff93f09466002bde745ea8d8
Rogi-Vokamre <126895550+Rogi-Vokamre@users.noreply.github.com>
2025-09-11 13:06:45 +0300

    Update FirstPage.js

--- mini-app/src/components/FirstPage/FirstPage.js
+++ mini-app/src/components/FirstPage/FirstPage.js
@@ -3,16 +3,15 @@ import RingAnimation from "../RingAnimation/RingAnimation";
 import TypingText from "../TypingText/TypingText";
 import "./FirstPage.css";
 
-function FirstPage({ onNext }) {
+function FirstPage({ onNext, t }) {
   return (
     <>
       <RingAnimation />
       <TypingText />
       <button className="next-button" onClick={onNext}>
-        Next
+        {t("nextButton")}
       </button>
 
-      {/* Спонсор — отображается сразу */}
       <div className="sponsor-container">
         <span className="sponsor-text">При поддержке АО "Шард"</span>
         <img
